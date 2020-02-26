1b724633dba1a136e7abae163b3482366ba4c510
藍奕濡 <a78945612385238@gmail.com>
2020-02-26 19:04:54 +0800

    20200226

--- js/vendor.js
+++ js/vendor.js
@@ -11,7 +11,6 @@
 $(document).ready(function() {
   $('.productCard__icon').on('click', function(e) {
     const icon = $(this).find('i');
-    console.log($(this));
     if (icon.hasClass('far')) {
       icon.removeClass('far').addClass('fas');
     } else {
