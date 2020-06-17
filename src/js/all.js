12f1a14bfcfbd4f4df86d822c10e4a7e750bed41
藍奕濡 <a78945612385238@gmail.com>
2020-06-18 03:21:12 +0800

    bug fix

--- src/js/all.js
+++ src/js/all.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 });
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(
+  navigator.serviceWorker.register('/bootstrap_layout/sw.js').then(
     function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
